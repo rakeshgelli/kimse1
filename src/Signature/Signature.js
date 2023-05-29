33a98ef56d64681008da9ca8410a4b21c44e294d
rakeshgelli <rakeshgelli.255@gmail.com>
2023-05-29 15:45:18 +0530

    commit message

--- src/Signature/Signature.js
+++ src/Signature/Signature.js
@@ -17,6 +17,8 @@ const Signature=()=>{
           }
     }
     return(
+        <div>
+        <h1>HI</h1>
         <div style={{height:'500px',width:'500px',border:'2px solid black',marginLeft:'500px',marginTop:'200px'}}>
             <SignatureCanvas ref={data =>setSign(data)} 
             canvasProps={{ width: '500px', height: '500px' }} />
@@ -27,6 +29,7 @@ const Signature=()=>{
           <img src={signatureImage} alt="Signature" style={{ maxWidth: '300px' }} />
         </div>
       )}
+        </div>
         </div>
     )
 }
